c7e9548cb9a900e37c33197df4ce298362f4c830
Jess2039 <jessjamain@free.fr>
2024-06-11 16:43:24 +0200

    home css

--- app/src/pages/Home.svelte
+++ app/src/pages/Home.svelte
@@ -3,4 +3,12 @@
         <h1>Jessica JAMAIN</h1>
             <h2>Développeur web - Intégrateur</h2>
     </div>
+    <section class="rsp_section">
+        <div class="border_white"></div>
+        <div class="rsp">
+            <a href=""><img src="/images/linkedin.png" class="linkedin_logo" alt="linkedin"></a>
+            <a href=""><img src="/images/github.png" class="github_logo" alt="github"></a>
+        </div>
+        <div class="border_white"></div>
+    </section>
 </main>    
